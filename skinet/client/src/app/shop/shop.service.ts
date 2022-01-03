4e5589d75ad6101d03af9e091192378fd9eba0fe
abraham <abrahamd@hotmail.com>
2022-01-02 20:57:23 -0500

    refactoring. routing,lazyloading

--- skinet/client/src/app/shop/shop.service.ts
+++ skinet/client/src/app/shop/shop.service.ts
@@ -5,6 +5,7 @@ import { IType } from '../shared/models/IProductType';
 import { IPagination } from '../shared/models/pagination';
 import { map } from 'rxjs/operators';
 import { ShopParams } from '../shared/models/shopParams';
+import { IProduct } from '../shared/models/product';
 
 @Injectable({
   providedIn: 'root'
@@ -35,6 +36,10 @@ export class ShopService {
       .pipe(map(response => {
         return response.body;
       }));
+  }
+
+  getProduct(id: number) {
+    return this.http.get<IProduct>(this.baseUrl + 'products/' + id);
   }
 
   getBrands() {
